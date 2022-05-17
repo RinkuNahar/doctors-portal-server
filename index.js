47a64efbd9c4c797fded50e4e232c1e1a8eaf4c1
RinkuNahar <rinkunaharhossan2022@gmail.com>
2022-05-17 21:47:21 +0600

    user booked added

--- index.js
+++ index.js
@@ -16,15 +16,37 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-       const servicesCollection = client.db("doctors_portal").collection('services');
+        const servicesCollection = client.db("doctors_portal").collection('services');
+        const bookingsCollection = client.db("doctors_portal").collection('bookings');
 
-    //    services.json er data newar jonno
-    app.get('/service', async(req,res)=>{
-        const query = {};
-        const cursor = servicesCollection.find(query);
-        const services = await cursor.toArray();
-        res.send(services);
-    })
+        //    services.json er data newar jonno
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        // API Naming Covention
+        // app.get('/booking) -> get all bookings or more than one or by query
+        // app.get('/booking/:id) -> get a specific booking
+        // app.post('/booking) -> add a new booking
+        // app.patch('/booking/:id') -> update specific
+        // app.delete('/booking/:id') -> delete specific
+
+        // Add a new booking:user j booking dicche setar information pabo
+        
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
+            const exists = await bookingsCollection.findOne(query);
+            if (exists) {
+              return res.send({ success: false, booking: exists })
+            }
+            const result = await bookingsCollection.insertOne(booking);
+            return res.send({ success: true, result });
+          });
+
     }
     finally {
 
