e9f4d42811ff8fedd3ff7caa1884555596bd8059
RinkuNahar <rinkunaharhossan2022@gmail.com>
2022-05-19 23:03:10 +0600

    user admin conditions applied

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
@@ -12,6 +13,21 @@ const port = process.env.PORT || 5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fyytq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+      if (err) {
+        return res.status(403).send({ message: 'Forbidden access' })
+      }
+      req.decoded = decoded;
+      next();
+    });
+  }
 
 async function run() {
     try {
@@ -28,17 +44,53 @@ async function run() {
             res.send(services);
         });
 
+        // sb gulo user k pabe
+        app.get('/user', verifyJWT, async(req,res)=>{
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        })
+
         // google dye sign in korle ageer thekei sign in kora chilo naki na seta janar jonno
-        app.put('/user/:email', async(req,res)=>{
+        app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
-            const filter = {email: email};
-            const options = {upsert : true};
+            const filter = { email: email };
+            const options = { upsert: true };
             const updateDoc = {
                 $set: user,
-              };
-              const result = await userCollection.updateOne(filter, updateDoc, options);
-              res.send(result);
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            //   JAWT Token
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+            res.send({ result, token });
+        });
+
+        // admin na hole all users dashboard er ta access i korte parbe na
+        app.get('/admin/:email', async(req,res)=>{
+            const email = req.params.email;
+            const user = await userCollection.findOne({email: email});
+            const isAdmin = user.role ==='admin';
+            res.send({admin: isAdmin});
+        })
+
+        // user er email theke admin pabo
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({email: requester});
+            if(requesterAccount.role === 'admin'){
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: {role: 'admin'},
+                };
+                const result = await userCollection.updateOne(filter, updateDoc, );
+                
+                res.send( result );
+            }
+            else{
+                res.status(403).send({message: 'forbidden'});
+            }
+           
         });
 
 
@@ -78,11 +130,18 @@ async function run() {
         // app.delete('/booking/:id') -> delete specific
 
         // dashboard e tar appointment gulo dekhte parbe user
-        app.get('/booking', async (req, res) => {
+        app.get('/booking', verifyJWT, async (req, res) => {
             const patient = req.query.patient;
-            const query = { patient: patient };
-            const bookings = await bookingsCollection.find(query).toArray();
-            res.send(bookings);
+            const decodedEmail = req.decoded.email;
+            if (patient === decodedEmail) {
+              const query = { patient: patient };
+              const bookings = await bookingsCollection.find(query).toArray();
+              return res.send(bookings);
+            }
+            else {
+              return res.status(403).send({ message: 'forbidden access' });
+            }
+          
         })
 
         // Add a new booking:user j booking dicche setar information pabo
